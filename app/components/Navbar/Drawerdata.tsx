ce4b02c7ec4ff4882c9fbae7c2348bd0ffc629b2
AKGADHAFI <gadhafi06032003@gmail.com>
2024-08-11 07:41:27 -0700

    first commit

--- app/components/Navbar/Drawerdata.tsx
+++ app/components/Navbar/Drawerdata.tsx
@@ -10,10 +10,10 @@ interface NavigationItem {
 
 const navigation: NavigationItem[] = [
     { name: 'Home', href: '#home-section', current: false },
-    { name: 'About us', href: '#about-section', current: false },
+    { name: 'About Us', href: '#about-section', current: false },
     { name: 'Recipe', href: '#cook-section', current: false },
     { name: 'Gallery', href: '#gallery-section', current: false },
-    { name: 'Contact', href: '#contact-section', current: false },
+    { name: 'Contact Us', href: '#contact-section', current: false },
 ]
 
 
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -39,13 +39,13 @@ const Navbar = () => {
                             {/* LOGO */}
 
                             <div className="flex sm:hidden flex-shrink-0 items-center border-right">
-                                <Image src="/images/Logo/Logo.svg" alt="logo" width={36} height={36} />
+                                <Image src="/images/Logo/logoo.svg" alt="logo" width={36} height={36} />
                                 <Link href="/" className='text-2xl font-semibold text-black ml-4'>
                                 J J Black Forest
                                 </Link>
                             </div>
                             <div className="hidden sm:flex flex-shrink-0 items-center border-right">
-                                <Image src="/images/Logo/Logo.svg" alt="logo" width={56} height={56} />
+                                <Image src="/images/Logo/logoo.svg" alt="logo" width={56} height={56} />
                                 <Link href="/" className='text-2xl font-semibold text-black ml-4'>
                                     J J Black Forest
                                 </Link>
