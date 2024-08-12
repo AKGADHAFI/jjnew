9144c3b62ac2f551aa6d43a6ffec7303b83c3329
AKGADHAFI <gadhafi06032003@gmail.com>
2024-08-12 00:08:21 -0700

    first commit

--- app/components/Work/index.tsx
+++ app/components/Work/index.tsx
@@ -76,11 +76,11 @@ const Work = () => {
                         {cardData.map((items, i) => (
                             <div className='card-b p-8 relative rounded-3xl' key={i}>
                             <div className='work-img-bg rounded-full flex justify-center absolute top-[-50%] sm:top-[-40%] md:top-[-55%] lg:top-[-45%] left-1/2 transform -translate-x-1/2'>
-                            <Image src={items.imgSrc} alt={items.imgSrc} width={170} height={140} />
+                            <Image src={items.imgSrc} alt={items.imgSrc} width={150} height={140} />
                         </div>
                         
                                 <h3 className='text-2xl text-black font-semibold text-center mt-16'>{items.heading}</h3>
-                                <p className='text-lg font-normal text-black text-center text-opacity-50 mt-2'>{items.subheading}</p>
+                                <p className='text-lg font-normal text-black text-center text-opacity-50 mt-2'>{items.subheading} <br /> <br /></p>
                                 <div className='flex items-center justify-center'>
                                     {/* <Link href='/'><p className='text-center text-lg font-medium text-pink mt-2 hover-underline'>{items.link}</p></Link> */}
                                 </div>
