24235a60836b32bd9e4e1312564479883066b179
AKGADHAFI <gadhafi06032003@gmail.com>
2024-08-14 06:47:57 -0700

    first commit

--- app/components/Gallery/index.tsx
+++ app/components/Gallery/index.tsx
@@ -2,6 +2,242 @@
 import Image from 'next/image';
 import { Fade } from "react-awesome-reveal";
 
+const images = [
+    {
+        src: "/images/Gallery/img1.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img2.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img3.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img4.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img5.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img6.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img7.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img8.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img9.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img10.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img11.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img12.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img13.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img14.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img15.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img16.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img17.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img18.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img19.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img20.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img21.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img22.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img23.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img24.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img25.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img26.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img27.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img28.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img29.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img30.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img31.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img32.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img33.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img34.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img35.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img36.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img37.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img38.jpg",
+        alt: "pizza-one",
+        width: 700,
+        height: 405,
+    },
+    {
+        src: "/images/Gallery/img39.jpg",
+        alt: "pizza-two",
+        width: 700,
+        height: 405,
+    },
+];
 
 const Gallery = () => {
     return (
@@ -13,34 +249,17 @@ const Gallery = () => {
                     </Fade>
                     <Fade direction={'up'} delay={800} cascade damping={1e-1} triggerOnce={true}>
                         <h3 className="text-3xl lg:text-5xl font-semibold text-black">
-                            Gallery of our Backery.
+                            Gallery of our Bakery.
                         </h3>
                     </Fade>
-
                 </div>
 
-                <div className='grid grid-cols-1 md:grid-cols-12 my-16 sm:space-x-6 space-y-6 md:space-y-0 px-6'>
-
-                    <div className='col-span-6 flex justify-center overflow-hidden rounded-3xl'>
-                        <Image src="/images/Gallery/foodone.jpg" alt="pizza-one" width={1000} height={805} className="inner-img"/>
-                    </div>
-
-                    <div className='col-span-6 flex justify-center'>
-                        <div className="grid grid-rows-1 grid-flow-row gap-4">
-                            <div className="row-span-1 overflow-hidden rounded-3xl">
-                                <Image src="/images/Gallery/foodtwo.jpg" alt="pizza-two" width={700} height={405} className="inner-img"/>
-                            </div>
-                            <div className='grid grid-cols-2 gap-2'>
-                                <div className='overflow-hidden rounded-3xl'>
-                                    <Image src="/images/Gallery/foodthree.jpg" alt="pizza-three" width={500} height={405} className="inner-img"/>
-                                </div>
-                                <div className='overflow-hidden rounded-3xl'>
-                                    <Image src="/images/Gallery/foodfour.jpg" alt="pizza-four" width={500} height={405} className="inner-img"/>
-                                </div>
-                            </div>
+                <div className='grid grid-cols-2 md:grid-cols-3 gap-6 my-16 px-6'>
+                    {images.map((image, index) => (
+                        <div key={index} className='flex justify-center overflow-hidden rounded-3xl'>
+                            <Image src={image.src} alt={image.alt} width={image.width} height={image.height} className="inner-img"/>
                         </div>
-                    </div>
-
+                    ))}
                 </div>
             </div>
         </div>
